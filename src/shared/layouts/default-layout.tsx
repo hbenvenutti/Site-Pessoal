dfd267382bf855cb8622aa32fea723cfd8577e34
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-02-11 16:22:42 -0300

    feat(layout): adicionada estilização ao layout padrao

--- src/shared/layouts/default-layout.tsx
+++ src/shared/layouts/default-layout.tsx
@@ -1,7 +1,16 @@
 import { Outlet } from 'react-router-dom';
 
+import { Sidebar } from '../components/sidebar';
+
+import { Container } from './styles';
+
 import type { ReactElement } from 'react';
 
 // * ------------------------------------------------------------------------------------------ * //
 
-export const DefaultLayout = (): ReactElement => <Outlet />;
+export const DefaultLayout = (): ReactElement => (
+  <Container>
+    <Sidebar />
+    <Outlet />
+  </Container>
+);
