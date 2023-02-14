232c1417b76757809614532476f2df4c02d4d1f6
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-02-14 15:12:59 -0300

    feat(mobile menu): agora o menu fecha ao mudar a página

--- src/shared/components/mobile-menu/index.tsx
+++ src/shared/components/mobile-menu/index.tsx
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
+import { useMobileMenu } from '../../hooks/mobile-menu-context';
+
 import { MobileMenuWrapper } from './styles';
 import { MobileLanguageSelector } from './mobile-language-selector';
 
@@ -8,14 +10,43 @@ import type { ReactElement } from 'react';
 // * ------------------------------------------------------------------------------------------ * //
 
 export const MobileMenu = (): ReactElement => {
+  // *** --- Contexts ----------------------------------------------------------------------- *** //
+  const { closeMobileMenu } = useMobileMenu();
+
+  // *** --- TSX ---------------------------------------------------------------------------- *** //
   return (
     <MobileMenuWrapper>
       <MobileLanguageSelector />
-      <NavLink to="/">Sobre</NavLink>
-      <NavLink to="/">Projetos</NavLink>
-      <NavLink to="/education">Educação</NavLink>
-      <NavLink to="/experience">Experiência</NavLink>
-      <NavLink to="/certificates">Certificados</NavLink>
+      <NavLink
+        onClick={closeMobileMenu}
+        to="/"
+      >
+        Sobre
+      </NavLink>
+      <NavLink
+        onClick={closeMobileMenu}
+        to="/"
+      >
+        Projetos
+      </NavLink>
+      <NavLink
+        onClick={closeMobileMenu}
+        to="/education"
+      >
+        Educação
+      </NavLink>
+      <NavLink
+        onClick={closeMobileMenu}
+        to="/experience"
+      >
+        Experiência
+      </NavLink>
+      <NavLink
+        onClick={closeMobileMenu}
+        to="/certificates"
+      >
+        Certificados
+      </NavLink>
     </MobileMenuWrapper>
   );
 };
