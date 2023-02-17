7f10818f336b0394d13b4dd2d7964c90178e60a8
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-02-16 21:14:17 -0300

    feat(education provider): adicionado mais um item de educação

--- src/shared/providers/education.ts
+++ src/shared/providers/education.ts
@@ -1,13 +1,33 @@
 import type { EducationList } from '../../@types/providers/education';
 
-// * ------------------------------------------------------------------------------------------ * //
+// * ---------------------------------------------------------------------- * //
 
 export const educationList: EducationList = [
   {
     institution: 'Instituto Federal do Rio Grande do Sul - Campus Restinga',
     course: 'Tecnólogo em Análise e Desenvolvimento de Sistemas',
-    startDate: new Date(2022, 6), // Month is an array index, 0 === january etc... so 6 === july.
+    startDate: new Date(2022, 6),
     finishDate: 'Cursando',
     location: 'Porto Alegre/RS'
+  },
+  {
+    institution: 'Instituto Federal Catarinense - Campus Araquari',
+    course: 'Bacharelado em Sistemas de Informação',
+    startDate: new Date(2019, 1),
+    finishDate: 'Interrompido',
+    location: 'Araquari/SC'
+  },
+  {
+    institution: 'Instituto Marista Nossa Senhora das Graças',
+    course: 'Ensino Médio',
+    startDate: new Date(2010, 1),
+    finishDate: new Date(2012, 11),
+    location: 'Viamão/RS'
   }
 ];
+
+/*
+   ? --- Date Month Index --------------------------------------------------- // 
+   ? Month is an array index -> 0 === January, 7 !== July.
+   ? 6 === July.
+*/
