4b370e27b8642a1bcbbe1f27feb600784f7789ba
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-02-23 22:22:51 -0300

    refactor(homepage): escondida a home, agora o site abre na pagina about

--- src/shared/routes/index.tsx
+++ src/shared/routes/index.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import { DefaultLayout } from '../layouts/default-layout';
-import { HomePage } from '../../pages/home';
+// import { HomePage } from '../../pages/home';
 import { CertificatesPage } from '../../pages/certificates';
 import { ExperiencePage } from '../../pages/experience';
 import { EducationPage } from '../../pages/education';
@@ -20,7 +20,7 @@ export const Router = (): ReactElement => (
     >
       <Route
         path="/"
-        element={<HomePage />}
+        element={<AboutPage />}
       />
 
       <Route
