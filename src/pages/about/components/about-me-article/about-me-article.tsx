ba3791841c56a5b98b59a23fd4fbcee45eaa8d4f
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-03-03 17:12:07 -0300

    feat(artigos): alteração no texto dos artigos

--- src/pages/about/components/about-me-article/about-me-article.tsx
+++ src/pages/about/components/about-me-article/about-me-article.tsx
@@ -9,28 +9,32 @@ function AboutMeArticle(): ReactElement {
         <h1>Sobre mim</h1>
         <article>
           <p>
-            Me chamo Huam Benvenutti, sou um desenvolvedor full stack. Curso Análise e
-            desenvolvimento de sistemas no Instituto Federal do Rio Grande do Sul (IFRS). Sou
-            apaixonado por tecnologia e estou sempre atento às notícias relacionadas ao campo.
+            Me chamo Huam Benvenutti, sou um desenvolvedor full stack. Curso
+            Análise e desenvolvimento de sistemas no Instituto Federal do Rio
+            Grande do Sul (IFRS). Sou apaixonado por tecnologia e estou sempre
+            atento às notícias relacionadas ao campo.
           </p>
 
           <p>
-            Minha especialidade é Typescript. Dei prioridade ao Javascript por ser uma linguagem
-            coringa, que pode-se fazer de tudo e mais um pouco. Na minha opinião o Typescript agrega
-            muito ao Javascript, gosto de tipar as variáveis e saber exatamente o que se envia e
-            recebe entre as funções. Sem falar que a IDE (VSCode) ganha super poderes ao utilizar-se
-            tipagem.
+            Minha especialidade é Typescript. Dei prioridade ao Javascript por
+            ser uma linguagem coringa, que pode-se fazer de tudo e mais um
+            pouco. Na minha opinião o Typescript agrega muito ao Javascript,
+            gosto de tipar as variáveis e saber exatamente o que se envia e
+            recebe entre as funções. Sem falar que a IDE (VSCode) ganha super
+            poderes ao utilizar-se tipagem.
           </p>
           <p>
-            Por gostar muito de Typescript, eu fico de olho no Deno. Poder criar aplicações sem
-            precisar transpilar o código é algo que me interessa bastante. Espero que a ferramenta
-            siga crescendo e a comunidade acabe tão forte quanto o próprio Node.
+            Por gostar muito de Typescript, eu fico de olho no Deno. Poder criar
+            aplicações sem precisar transpilar o código, é algo que me interessa
+            bastante. Espero que a ferramenta siga crescendo e a comunidade
+            acabe tão forte quanto o próprio Node.
           </p>
           <p>
-            Meu primeiro contato com programação foi na faculdade, lidei com Java, C, Python e
-            Javascript. Contudo, os conhecimentos avançados eu adquiri com a Rocketseat. Comprei o
-            curso deles anos atrás. E por ser um dos primeiros, me deram acesso vitalício a todas as
-            atualizações de conteúdo.
+            Meu primeiro contato com programação foi na faculdade, lidei com
+            Java, C, Python e Javascript. Contudo, os conhecimentos avançados,
+            eu adquiri com a Rocketseat. Comprei o curso deles anos atrás. E por
+            ser um dos primeiros, tenho acesso vitalício a todas as atualizações
+            de conteúdo.
           </p>
         </article>
       </div>
@@ -39,18 +43,20 @@ function AboutMeArticle(): ReactElement {
         <h2>Faculdade</h2>
         <article>
           <p>
-            Eu comecei meus estudos no Instituto Federal de Santa Catarina no município de
-            Caçador/SC. Fazia bacharelado em sistemas de informação. Meus familiares precisaram se
-            mudar para Joinville e eu transferí a minha matrícula para o Instituto Federal
-            Catarinense de Araquarí/SC, cidade satelite de Joinville.
+            Eu comecei meus estudos no Instituto Federal de Santa Catarina no
+            município de Caçador/SC. Fazia bacharelado em sistemas de
+            informação. Por motivos pessoais eu transferí a minha matrícula para
+            o Instituto Federal Catarinense de Araquarí/SC, cidade satelite de
+            Joinville.
           </p>
           <p>
-            Quando a pandemia começou, acabei trancando a minha matrícula e acabei não voltando para
-            a faculdade. Após um ano sem estudar, tentei me transferir novamente, dessa vez para
-            Porto Alegre/RS, minha cidade natal. Não consegui a transferência e tive que prestar
-            vestibular. Resultado, passei em primeiro lugar e estou cursando desde então análise e
-            desenvolvimento de sistemas no Instituto Federal do Rio Grande do Sul no câmpus
-            restinga.
+            Quando a pandemia começou, acabei trancando a minha matrícula e
+            acabei não voltando para a faculdade. Após um ano sem estudar,
+            tentei me transferir novamente, dessa vez para Porto Alegre/RS,
+            minha cidade natal. Não consegui a transferência e tive que prestar
+            vestibular. Resultado, passei em primeiro lugar e estou cursando
+            desde então análise e desenvolvimento de sistemas no Instituto
+            Federal do Rio Grande do Sul no câmpus restinga.
           </p>
         </article>
       </div>
